0004260ff5d54e2c375d7e37869f7f285ee6569e
ben <jwben03@gmail.com>
2026-04-29 19:01:35 +0800

    commit the main file to enable running on cloud run

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 import os
 from dotenv import load_dotenv
 from bus_botback import process_update_from_webhook
 import uvicorn
+import base64
 import httpx
 
 load_dotenv()
 
-# app = FastAPI()
+app = FastAPI()
 
 BOT_TOKEN = os.getenv("TELE_TOKEN")
 CLOUD_RUN_BASE_URL = os.getenv("WEBHOOK_URL")
-WEBHOOK_URL = f"{CLOUD_RUN_BASE_URL}/webhook"
+WEBHOOK_URL = f"{CLOUD_RUN_BASE_URL}/{BOT_TOKEN}"
 
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
 print("📢 Loaded BOT_TOKEN:", BOT_TOKEN[:10] + "..." if BOT_TOKEN else "None")
 
 # Setup Google Sheets credentials for Cloud Run
-# def setup_google_credentials():
-#     # Check if running on Cloud Run (has GOOGLE_CREDS_BASE64)
-#     if os.getenv("GOOGLE_CREDS_BASE64"):
-#         print("🔧 Setting up Google credentials from environment...")
-#         with open("credentials.json", "wb") as f:
-#             f.write(base64.b64decode(os.getenv("GOOGLE_CREDS_BASE64")))
-#         print("✅ Google credentials file created")
-#     elif os.path.exists("credentials.json"):
-#         print("✅ Found local credentials.json file")
-#     else:
-#         print("⚠️ No Google credentials found")
+def setup_google_credentials():
+    # Check if running on Cloud Run (has GOOGLE_CREDS_BASE64)
+    if os.getenv("GOOGLE_CREDS_BASE64"):
+        print("🔧 Setting up Google credentials from environment...")
+        with open("credentials.json", "wb") as f:
+            f.write(base64.b64decode(os.getenv("GOOGLE_CREDS_BASE64")))
+        print("✅ Google credentials file created")
+    elif os.path.exists("credentials.json"):
+        print("✅ Found local credentials.json file")
+    else:
+        print("⚠️ No Google credentials found")
 
-# # Setup credentials on startup
-# setup_google_credentials()
+# Setup credentials on startup
+setup_google_credentials()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with httpx.AsyncClient() as client:
-        response = await client.post (
-            TELEGRAM_API_URL,
-            json={"url": WEBHOOK_URL}
-        )
-        print("Webhook set:", response.status_code, response.json())
-    yield
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     async with httpx.AsyncClient() as client:
+#         response = await client.post (
+#             TELEGRAM_API_URL,
+#             json={"url": WEBHOOK_URL}
+#         )
+#         print("Webhook set:", response.status_code, response.json())
+#     yield
 
-app = FastAPI(lifespan=lifespan)
+# app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
